097c2a7199626863f82709728846bdf73c7254f6
parvinder <parvinder.ghotra@gmail.com>
2014-03-30 13:58:14 -0400

    [0] adds project scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gray::Application.config.secret_key_base = '8aa2bb4e16ae40c2a8e5c777c7690392c97cd38ff6d1a1d6b575445842f16c202938a419b1757b3538ffeb388fce983b42d2e5a16ff5a001c996f9bdb05ef837'
