4ed646bef7e4d4956dc156239e28e19f633d54cc
parvinder <parvinder.ghotra@gmail.com>
2014-03-31 15:51:23 -0400

    refactors case list views

--- spec/controllers/cases_controller_spec.rb
+++ spec/controllers/cases_controller_spec.rb
@@ -3,11 +3,29 @@ describe CasesController do
     render_views
 
     describe "when cases available" do
+      let(:mock_cases) do
+        [{"subject" => 'Fix-it-all', 'status' => 'open', 'type' => 'twitter'},
+         {"subject" => 'Test Case', 'status' => 'open', 'type' => 'email'}]
+      end
+
+      before(:each) do
+        Case.should_receive(:all).and_return(mock_cases)
+      end
+
+      it "renders table headers" do
+        get :index
+
+        response.body.should have_selector("#caseTable .table-header") do |header|
+          header.should have_selector(:span, content: "Subject")
+          header.should have_selector(:span, content: "Status")
+          header.should have_selector(:span, content: "Type")
+        end
+      end
 
       it "should render entries case table" do
         get :index
 
-        response.body.should have_selector("#caseTable") do |cases|
+        response.body.should have_selector("#caseTable .table-content") do |cases|
           cases.should have_selector(:span, content: 'Fix-it-all')
         end
       end
