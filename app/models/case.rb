0b5284304fbfc89708da63f003b0b3458883bdb3
parvinder <parvinder.ghotra@gmail.com>
2014-04-01 08:53:40 -0400

    changes Case.get to return collection cases objects

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,8 +1,46 @@
 class Case < DeskApiBase
+  extend DefineAttrs
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+
+  define_accessors [:subject, :status, :type, :priority, :labels, :label_action, :id]
 
   ENDPOINT = "api/v2/cases"
 
-  def self.all
-    get(ENDPOINT, EmbeddedEntriesParser)
+  def initialize(attrs={})
+    set_attributes(attrs)
   end
+
+  def self.all
+    translate_to_cases get(ENDPOINT, EmbeddedEntriesParser)
+  end
+
+  def persisted?
+    self.id.present?
+  end
+
+  def new_record?
+    !persisted?
+  end
+
+  private
+
+  def set_attributes(attrs)
+    attrs.each_pair do |key, value|
+      if self.respond_to?(attribute = "#{key}=".to_sym)
+        self.send(attribute, value)
+      end
+    end
+  end
+
+  def self.translate_to_cases(json_set)
+    [].tap do |case_items|
+      if json_set.present?
+        json_set.each do |json|
+          case_items << new(json)
+        end
+      end
+    end
+  end
+
 end
--- spec/models/case_spec.rb
+++ spec/models/case_spec.rb
@@ -5,5 +5,22 @@ describe Case do
 
       Case.all
     end
+
+    it "returns a collection of case objects" do
+      case_response = [{"subject" => "a test case", "status"=>"open",
+                        "type" => "email", "priority" => 9, "id" => 32},
+                       {"subject" => "another test case", "status"=>"open",
+                        "type" => "twitter", "priority" => 9, "id" => 77}]
+
+      Case.should_receive(:get)
+          .with("api/v2/cases", EmbeddedEntriesParser)
+          .and_return(case_response)
+
+     case_items = Case.all
+
+     case_items.should have(2).items
+     case_items.first.should be_kind_of(Case)
+     case_items.first.id.should == 32
+    end
   end
 end
